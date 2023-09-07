631d7ff58d55ad147991ff4af9b8db2e85f89b3c
Ashwani kumar <rathiashwani8@gmail.com>
2023-09-07 14:29:05 +0530

    set up structure

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -7,14 +7,14 @@ from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
 @dataclass
-class DataIngestionConfiy:
+class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
     raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfiy()
+        self.ingestion_config=DataIngestionConfig()
 
     def intiate_data_ingestion(self):
         try:
